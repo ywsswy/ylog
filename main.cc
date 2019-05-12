9d4f1ebbd6c804a357e9a9740788d461fc50ff26
yws <ql1119079560@yahoo.com>
2019-05-12 13:21:21 +0800

    change function_name to upper W

--- main.cc
+++ main.cc
@@ -36,12 +36,12 @@ int main(){
   double b = 13.14;
   std::string c = "I love U.";
 
-  log1.w(__FILE__, __LINE__, YLog::INFO, "watch_a",a);//INFO级别不低于log1的下限INFO级别，正常写入日志文件
-  log1.w(__FILE__, __LINE__, YLog::ERR, "see_b",b);//正常写入
-  log1.w(__FILE__, __LINE__, YLog::INFO, "log_c",c);//正常写入
+  log1.W(__FILE__, __LINE__, YLog::INFO, "watch_a",a);//INFO级别不低于log1的下限INFO级别，正常写入日志文件
+  log1.W(__FILE__, __LINE__, YLog::ERR, "see_b",b);//正常写入
+  log1.W(__FILE__, __LINE__, YLog::INFO, "log_c",c);//正常写入
 
-  log2.w(__FILE__, __LINE__, YLog::INFO, "A",a);//INFO级别低于log2的下限ERROR级别，不写入日志
-  log2.w(__FILE__, __LINE__, YLog::ERR, "B",b);//正常写入
-  log2.w(__FILE__, __LINE__, YLog::INFO, "C",c);//不写入日志
+  log2.W(__FILE__, __LINE__, YLog::INFO, "A",a);//INFO级别低于log2的下限ERROR级别，不写入日志
+  log2.W(__FILE__, __LINE__, YLog::ERR, "B",b);//正常写入
+  log2.W(__FILE__, __LINE__, YLog::INFO, "C",c);//不写入日志
   return 0;
 }
